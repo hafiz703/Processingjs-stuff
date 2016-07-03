f102fa957d7b7c097c5852677a6f4fb265c0a8cf
hafiz703 <mhbt-703@hotmail.com>
2016-07-03 20:05:23 +0800

    Fractal p5js

---
+++ fractal/sketch.js
@@ -0,0 +1,57 @@
+var angle;
+var factor;
+var count;
+var anitype;
+var treeType
+function setup() {
+  createCanvas(1380,500)
+  sliderAngle = createSlider(0,TWO_PI,PI/4,0.01)
+  sliderAnim = createSlider(0,20,1,1);
+  sliderTreeType = createSlider(0,2,0,1);
+  factor = 0.67;
+  count=0;
+}
+
+function draw() {
+  background(0)
+
+  
+  translate(width/2,height)
+  angle = sliderAngle.value();
+  anitype=sliderAnim.value();
+  treeType = sliderTreeType.value();
+  branch(150);
+  
+}
+
+
+function branch(len){
+  count++;
+  stroke(len*random(10),len*random(10),len*random(10));
+  strokeWeight(len/10)
+  if(count%anitype===0){
+    if(treeType===0)line(0,0,0,-len)
+    else if (treeType===1)rect(0,0,0,-len)
+    else{
+      ellipse(0,0,0,-len)
+    }
+  }
+  //else{ellipse(0,0,0,-len)}
+  
+  translate(0,-len)
+  if(len>4){
+    push()
+    rotate(angle);
+    branch(len*factor)
+    pop()
+    push()
+    rotate(-angle);
+    branch(len*factor)
+    pop()
+
+    // push()
+    // branch(len*factor)
+    // pop()
+
+  }
+}
