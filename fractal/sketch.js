3bf122321cfdbc240248eb762b8a973aa5385f81
hafiz703 <mhbt-703@hotmail.com>
2016-07-03 20:58:29 +0800

    Added Brightness slider

--- fractal/sketch.js
+++ fractal/sketch.js
@@ -2,24 +2,26 @@ var angle;
 var factor;
 var count;
 var anitype;
-var treeType
+var treeType;
+var bright;
 function setup() {
   createCanvas(1380,500)
   sliderAngle = createSlider(0,TWO_PI,PI/4,0.01)
-  sliderAnim = createSlider(0,20,1,1);
+  sliderAnim = createSlider(1,20,1,1);
   sliderTreeType = createSlider(0,2,0,1);
+  sliderBrightness = createSlider(1,20,10,0.01)
   factor = 0.67;
   count=0;
 }
 
 function draw() {
-  background(0)
-
-  
+  background(0)  
   translate(width/2,height)
   angle = sliderAngle.value();
   anitype=sliderAnim.value();
   treeType = sliderTreeType.value();
+  bright = sliderBrightness.value();
+  
   branch(150);
   
 }
@@ -27,7 +29,7 @@ function draw() {
 
 function branch(len){
   count++;
-  stroke(len*random(10),len*random(10),len*random(10));
+  stroke(len*random(bright),len*random(bright),len*random(bright));
   strokeWeight(len/10)
   if(count%anitype===0){
     if(treeType===0)line(0,0,0,-len)
